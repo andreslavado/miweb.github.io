577e02d2e7f43f937a5b079fbb01e6a6a227e136
Proyecto Web Andres <andres_5520@hotmail.com>
2022-06-03 15:22:26 -0500

    modificando archivos

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let ubicacionPrincipal = window.pageYOffset;
+
+window.addEventListener("scroll", function(){
+    let desplazamientoActual = window.pageYOffset;
+    if(ubicacionPrincipal >= desplazamientoActual){
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal=desplazamientoActual;
+})
+
+//MENU
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".menu")[0].addEventListener("click", function(){
+    if(semaforo){
+        document.querySelectorAll(".menu")[0].style.color ="#fff";
+        semaforo = false;
+    }else{
+        document.querySelectorAll(".menu")[0].style.color ="#000";
+        semaforo=true
+    }
+    
+    enlacesHeader.classList.toggle("menudos")
+})
